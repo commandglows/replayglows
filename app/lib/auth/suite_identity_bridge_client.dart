c4bbb5b545b054ff64deeebcfccbce18721f5105
Diane <35034946+dianedef@users.noreply.github.com>
2026-09-04 23:48:08 +0200

    fix(app): adopt Dart 3.12 initializing formal

--- app/lib/auth/suite_identity_bridge_client.dart
+++ app/lib/auth/suite_identity_bridge_client.dart
@@ -10,8 +10,7 @@ typedef FirebaseIdTokenResolver =
     Future<String?> Function({required bool forceRefresh});
 
 class SuiteIdentityBridgeClient {
-  const SuiteIdentityBridgeClient({http.Client? httpClient})
-    : _httpClient = httpClient;
+  const SuiteIdentityBridgeClient({this._httpClient});
 
   final http.Client? _httpClient;
 
